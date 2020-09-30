3cfa662b18a14d70ffb09fe7464a59ffb4d8e411
Alonzo Jackson <jaxonetic@gmail.com>
2020-09-30 10:40:22 -0700

    Initial Reboot Revision

---
+++ index.js
@@ -0,0 +1,159 @@
+/**
+ * @format
+
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+
+/**
+ *
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+*/
+import React, { Component } from "react";
+import { Image, SafeAreaView, } from "react-native";
+
+import { Root, Container,Header, Left,Content } from "native-base";
+import sideBarReducer from './src/redux/sideBarReducer.js';
+import { StyleSheet, View,Text,YellowBox,NativeEventEmitter, Platform, AppRegistry, NativeModules, AsyncStorage,StatusBar} from 'react-native';
+import { name as appName } from './app.json';
+
+
+import { initialStoreState } from './src/redux/state.js';
+import { Provider } from 'react-redux';
+import { createAppContainer } from 'react-navigation';
+import { rootSaga } from './src/redux/sagas/authSagas.js';
+import { createStore, applyMiddleware,combineReducers } from 'redux';
+import resourcesReducer from './src/redux/resourcesReducer.js';
+import videoRefsReducer from './src/redux/videoRefsReducer.js';
+import profilesReducer from './src/components/Profile/Redux/Reducers/profileReducer.js';
+import authReducer from './src/components/Authentication/Redux/Reducers/authReducer.js';
+import eventsReducer from './src/components/Event/Redux/Reducers/eventReducer.js';
+import createSagaMiddleware from 'redux-saga'
+import BottomNav from './src/components/NavBars/BottomNav.js';
+import FooterNav from './src/components/NavBars/footerNav.js';
+import SideBar from './src/components/NavBars/sidebar.js';
+
+import { Switch, Route, Redirect,Link } from 'react-router'
+import { NativeRouter } from 'react-router-native'
+
+import Trubrary from './src/components/Trubrary/trubrary';
+import YouTubeList from './src/components/Trubrary/youtubeList.js';
+import Activities from './src/components/Activities/Activities';
+import SimpleWebview from './src/components/WebResources/simpleWebView.js';
+import Home from './src/components/Home/home';
+import CalendarView from './src/components/calendarView';
+import MapView from './src/components/mapview';
+import ProfileView from './src/components/Profile/profileview';
+import EventView from './src/components/Event/eventView.js';
+import EventSearch from './src/components/Event/eventSearch';
+
+import { COMMON_DARK_BACKGROUND, COMMON_ICON_STYLE_SILVER, 
+        ROUTE_PROFILE_VIEW,ROUTE_EVENT_VIEW, ROUTE_YOUTUBELIST_VIEW,ROUTE_ROOT,
+         ROUTE_HOME, ROUTE_ACTIVITIES, ROUTE_TRUBRARY, ROUTE_MAPVIEW, 
+         ICON_TAG_MENU, iconManager} from './src/constants.js'
+
+// Logger with default options
+import logger from 'redux-logger';
+import SideMenu from 'react-native-side-menu';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ProfileSearch from './src/components/Profile/profileSearch.js'
+import { createDrawerNavigator, DrawerContentScrollView,
+  DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+//combine reducers
+const rootReducer = combineReducers({profiles: profilesReducer, events:eventsReducer, auth: authReducer, resourcesData:resourcesReducer, 
+  sideBar:sideBarReducer,videoMediaPromotions:videoRefsReducer});
+
+//create redux store
+const store = createStore(rootReducer, initialStoreState,  applyMiddleware(sagaMiddleware, logger) );
+
+
+//YellowBox.ignoreWarnings(['Warning: componentWillReceiveProps','Warning: componentWillUpdate', 'Warning: componentWillMount']);
+console.disableYellowBox = true;
+
+sagaMiddleware.run(rootSaga);
+
+
+DrawerImage = () =>{return(
+   <Container style={styles.container}><Content>    
+    <Image  style={styles.headerImageStyles}
+            source={{
+              uri:"https://i0.wp.com/www.experience-ancient-egypt.com/wp-content/uploads/2015/05/egyptian-goddess-maat.jpg"
+            }}/></Content></Container>)};
+
+
+function StackedHome() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+}
+
+function CustomDrawerContent(props) {
+
+  customProps = {...props, labelStyle:styles.customDrawerLabelStyle, activeBackgroundColor:"silver", inactiveBackgroundColor:"white", style:{backgroundColor:COMMON_DARK_BACKGROUND}};
+
+  return (
+    
+    <DrawerContentScrollView {...customProps}>
+    <DrawerImage />
+      <DrawerItemList  {...customProps} />
+      <DrawerItem {...customProps} label="Help" onPress={() => alert('Link to help')} />
+    </DrawerContentScrollView>
+  );
+}
+
+/*
+ * Redux-ed application entry point
+ */
+export default function Main() {
+  
+  return (<Root>
+  <SafeAreaView style={styles.safeArea}>
+  <Provider store={store}>
+    <NavigationContainer >
+      <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen  name="Home" component={StackedHome} />
+      <Drawer.Screen name="Trubrary" component={Trubrary} />
+      <Drawer.Screen name="Activities" component={Activities} />
+    </Drawer.Navigator>
+    </NavigationContainer>
+   </Provider>
+</SafeAreaView>
+   </Root>);
+  
+}
+
+
+const styles = StyleSheet.create({
+
+  customDrawerLabelStyle : {fontSize:24},
+  safeArea: { flex: 1} ,
+  headerImageStyles:{
+              height: 120,
+              width: 250,
+              position: "relative",
+              alignSelf: "stretch",
+              top: 10
+            },
+  container: {marginTop:30,
+    backgroundColor:COMMON_DARK_BACKGROUND, borderRadius: 14, alignItems: 'center',height:125
+  }
+});
+
+
+AppRegistry.registerComponent(appName, () => Main);
---
+++ src/services/servicesManager.web.js
@@ -0,0 +1,216 @@
+
+import {getRunningFunctionName,PROFILES_COLLECTION, EVENT_COLLECTION,
+        DBNAME,ATLAS_FACTORY,
+        FUNCTION_INSERTPROFILE,FUNCTION_INSERTEVENT, FUNCTION_QUERYPROFILE, FUNCTION_QUERYEVENTS, FUNCTION_UPDATEPROFILE,FUNCTION_UPDATEEVENT,
+        FUNCTION_RETRIEVE_GOOGLE_WEBCLIENTID, FUNCTION_RETRIEVE_GOOGLE_IOSCLIENTID, FUNCTION_RETRIEVE_GOOGLE_APIKEY,
+        GOOGLESIGNIN_OPTION_SCOPE, GOOGLESIGNIN_OPTION_RESPONSE_TYPE,REMOTE_RESOURCE_STRING} from  '../constants.js'
+import  { Stitch, RemoteMongoClient, AnonymousCredential, GoogleCredential,CustomCredential,UserPasswordCredential } from  'mongodb-stitch-browser-sdk';
+//import  { GoogleSignin } from 'react-native-google-signin';
+import  Geocoder from 'react-native-geocoding';
+/*import  {deleteManyEvents, deleteManyProfiles, deleteEvent, deleteProfile, 
+        updateSingleEvent, updateSingleProfile, insertSingleProfile,
+        insertSingleEvent, fetchEvents, fetchProfilesDB, getUserList} from './StitchCRUD_api.js'*/
+import  CrudService from "./stitchCRUD_api.js"
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+//  the Firebase products 
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/database";
+
+
+/**
+ *
+ * A Class to handle interaction with remote Stitch Mongo backend
+ *
+ * 
+ */
+ export default class ServicesManager {
+ 
+  static dbClient = async ()=> { 
+    const client =   Stitch.hasAppClient(REMOTE_RESOURCE_STRING) ? 
+    Stitch.defaultAppClient :
+        Stitch.initializeDefaultAppClient(REMOTE_RESOURCE_STRING);
+
+  return client;
+  }
+
+/**
+ * Initialize sub "services" like CRUD services
+ *
+ */
+  async initialize(){
+    
+    let client = await ServicesManager.dbClient(REMOTE_RESOURCE_STRING);
+    this.db = client.getServiceClient(RemoteMongoClient.factory, ATLAS_FACTORY).db(DBNAME);
+    this.stitchCrudServices = new CrudService(this.db, ServicesManager.dbClient);
+  
+  
+  var firebaseConfig = {
+    apiKey: "AIzaSyAE6KIft5wm1pcUMkgRV4Dsyu0L5fY1O4c",//"AIzaSyCSXLM0pMb0GCPvNtVdd1GeSa0UCl6zAEM",
+    authDomain: "gathering-f3e30.firebaseapp.com",
+    databaseURL: "https://gathering-f3e30.firebaseio.com",
+    projectId: "gathering",
+    storageBucket: "gathering.appspot.com",
+    messagingSenderId: "296133059037",
+    appId: "1:296133059037:web:613f88574f982937"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  console.log("Innialized", firebase);
+  }
+
+
+/**
+ * Privately retrieve the google keys required for google sign in and Geocoding API
+ * Ths method may become deprecated in the future when I move the GoogleSign to a central server
+ */
+async configureGoogleKeys(){
+  const client = await ServicesManager.dbClient();
+   const googleWebClientKey = await client.callFunction(FUNCTION_RETRIEVE_GOOGLE_WEBCLIENTID);
+   const googleIosClientKey = await client.callFunction(FUNCTION_RETRIEVE_GOOGLE_IOSCLIENTID);
+   const retrieveGoogleApiKey = await client.callFunction(FUNCTION_RETRIEVE_GOOGLE_APIKEY);
+   
+   // configure react-native-google-signin
+   //GoogleSignin.configure({scope:GOOGLESIGNIN_OPTION_SCOPE, response_type:GOOGLESIGNIN_OPTION_RESPONSE_TYPE, iosClientId:googleIosClientKey.secret, webClientId:googleWebClientKey.secret});
+   
+   // initialize Geocoder with
+   Geocoder.init(retrieveGoogleApiKey.secret);
+}
+
+/**
+ * Log in to Stitch backend anonymously
+ */
+ async  authorizeAnonymously() {
+
+  try{
+  let authorizedUser;
+    // Check if this user has already authenticated and we're here
+  const client = await ServicesManager.dbClient();
+
+  if (client.auth.isLoggedIn){
+    authorizedUser = client.auth.authInfo;
+  }
+  else{
+    authorizedUser = await  client.auth.loginWithCredential(new AnonymousCredential()) ;
+  }
+  
+  return  authorizedUser;
+ }catch(error){
+  return{errorStack:'authorizeAnonymously() => '+error};
+ }
+}
+
+/**
+* googleSignIn - create a GoogleCredential from the authCode and attempt to login to Stitch with it.
+* @param authCode (required) - the one-time/first-time serverAuthCode specified by google
+*/
+<<<<<<< HEAD
+ async  googleSignIn(googleObject) {
+console.log("googleSignin with authcode=", googleObject);
+=======
+ async  googleSignIn(authCode) {
+
+>>>>>>> parent of f1225d23... pre firebase branch
+  //parameter check
+  if(!googleObject) return null;
+  try{
+   let googleCredential = new GoogleCredential(googleObject);
+   const client = await ServicesManager.dbClient();
+
+  const authorizedUser = await client.auth.loginWithCredential(googleCredential);
+ 
+return authorizedUser;
+}catch(error){
+  return{errorStack:error};
+  }
+}
+
+   isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+ googleFirebaseSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  console.log('onsignin', googleUser);
+  console.log('Google Auth Response', googleUser);
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var fbUser = null;
+  var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+  fbUser=firebaseUser;
+
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    console.log(googleUser,"----", firebaseUser);
+    if (!this.isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.getAuthResponse().id_token);
+      // Sign in with credential from the Google user.
+      firebase.auth().signInWithCredential(credential).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    } else {
+        console.log('User already signed-in Firebase.');
+        // Get a reference to the database service
+      var database = firebase.database();
+  
+  var profiles =  firebase.database().ref('/profiles/'+ googleUser.getBasicProfile().getId());
+  console.log("profiles:",profiles);
+  
+ // function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('/profiles').push({
+    username: "name10",
+    email: "email10",
+    inner_id: googleUser.getBasicProfile().getId(),
+    profile_picture : profile.getImageUrl()
+  });
+//}
+ 
+    }
+  });
+  console.log("googlesignin firebaseuser=", fbUser);
+  return fbUser;
+}
+
+/**
+ * Log out using the client's client.auth.logout()
+ */
+ async  logout() {
+  try{
+// Now remove user1
+//await client.auth.removeUser();
+
+  const unAuthorizedUser = await this.client.auth.logout();
+ 
+return unAuthorizedUser;
+}catch(error){
+  return{errorStack:error};
+  }
+}
+
+
+}
